8bdd92913d0455f2026860b215f8495e5dd64c07
VITOR SILLOS ALONSO <vsa.alonso@gmail.com>
2023-08-04 10:40:29 -0300

    Create last_queries_ran.sql

---
+++ last_queries_ran.sql
@@ -0,0 +1,7 @@
+SELECT  t.[text], s.last_execution_time
+FROM sys.dm_exec_cached_plans AS p
+INNER JOIN sys.dm_exec_query_stats AS s
+   ON p.plan_handle = s.plan_handle
+CROSS APPLY sys.dm_exec_sql_text(p.plan_handle) AS t
+WHERE t.[text] LIKE N'%some text you are looking for%' 
+ORDER BY s.last_execution_time DESC;
